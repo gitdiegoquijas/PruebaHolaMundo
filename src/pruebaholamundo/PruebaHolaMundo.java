5f0e16c00a2d1a9694af9d02aba8a9a7c215e937
diego padilla <quijas.diego25@gmail.com>
2023-02-21 19:49:51 -0600

    Esta es mi primer versión

---
+++ src/pruebaholamundo/PruebaHolaMundo.java
@@ -0,0 +1,16 @@
+package pruebaholamundo;
+
+/**
+ *
+ * @author diego
+ */
+public class PruebaHolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+    
+}
